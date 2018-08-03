967c1526d34c5b32c103b4361f65e59eb3f70edf
AK <ak@thecarousell.com>
2018-08-03 11:01:22 +0800

    Correcting docker build script.

--- docker/build_and_upload_image.sh
+++ docker/build_and_upload_image.sh
@@ -6,5 +6,3 @@ docker build . -f ./docker/Dockerfile -t therako/bqsqoop:${VERSION}
 docker push therako/bqsqoop:${VERSION}
 docker build . -f ./docker/Dockerfile -t therako/bqsqoop:latest
 docker push therako/bqsqoop:latest
-
-mv -f ./docker/Dockerfile.bkp ./docker/Dockerfile
